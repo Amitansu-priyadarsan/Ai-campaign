73d1364942fb6793c32587c7fba702ae20be0fcb
Amitansu-priyadarsan <amitansupriyadarsan70@gmail.com>
2026-03-15 01:58:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-green-500 text-white text-center py-2 font-bold text-lg">
+      Tailwind CSS is working!
+    </div>
+  )
+}
+
+export default App
